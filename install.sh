b614cdb9de90fbacf380580bd478cf4c272b5f15
Destin Lee <dleeinc@gmail.com>
2025-03-07 11:43:23 -0600

    saving

--- install.sh
+++ install.sh
@@ -4,21 +4,10 @@ CWD="$(pwd)"
 HOME_DIR="$HOME"
 CONFIG_DIR="$HOME_DIR/.config"
 XDG_CONFIG_HOME="$CONFIG_DIR"
-LOCAL_BIN="$HOME_DIR/.local/bin"
+HOME_LOCAL_BIN="$HOME_DIR/.local/bin"
 
-# Create the .config directory if it doesn't exist
-if [ ! -d "$CONFIG_DIR" ]; then
-  echo "Creating $CONFIG_DIR"
-  mkdir -p "$CONFIG_DIR"
-fi
-
-# List of file names
-HOME_FILES=("vimrc" "bashrc" "aliases" "tmux.conf" "sqliterc")
-HOME_DIRS=("ghostty")
-
-CONFIG_DIRS=("nvim" "aerospace")
-
-# Function to create a symbolic link
+# functions
+## Function to create a symbolic link
 create_symlink() {
   local target="$1"
   local link="$2"
@@ -35,29 +24,38 @@ create_symlink() {
   fi
 }
 
+# Create the .config directory if it doesn't exist TODO: don't need this as -p work below
+if [ ! -d "$CONFIG_DIR" ]; then
+  echo "Creating $CONFIG_DIR"
+  mkdir -p "$CONFIG_DIR"
+fi
+
+# List of file names
+HOME_FILES=("vimrc" "bashrc" "aliases" "tmux.conf" "sqliterc")
+HOME_DIRS=()
+CONFIG_DIRS=("nvim" "ghostty" "aerospace")
+BIN_FILES=("d-ready-parent" "doit" "newproj")
+
 # Loop through the list and create symlinks
 for file in "${HOME_FILES[@]}"; do
-  create_symlink "$CWD/$file" "$HOME_DIR/.$file"
+  create_symlink "$CWD/home/$file" "$HOME_DIR/.$file"
 done
 
 # Loop through the list of directories and create symlinks
 for dir in "${HOME_DIRS[@]}"; do
-  create_symlink "$CWD/$dir" "$CONFIG_DIR/$dir"
+  create_symlink "$CWD/home/$dir" "$CONFIG_DIR/$dir"
 done
 
 # Loop through the list of config directories and create symlinks
 for dir in "${CONFIG_DIRS[@]}"; do
   if [ ! -d "$CONFIG_DIR/$dir" ]; then
-    create_symlink "$CWD/$dir" "$CONFIG_DIR/$dir"
+    create_symlink "$CWD/config/$dir" "$CONFIG_DIR/$dir"
   else
     echo "$dir already exists as a directory!"
   fi
 done
 
-## TODO: Add support for creating symlinks for directories
-
-if [ ! -f "$HOME_DIR/bin/tome.sh" ]; then
-  create_symlink "$CWD/tome.sh" "$LOCAL_BIN/tome"
-else
-  echo "tome is already installed"
-fi
+# Loop through the list of bin files and create symlinks
+for file in "${BIN_FILES[@]}"; do
+  create_symlink "$CWD/bin/$file" "$HOME_LOCAL_BIN/$file"
+done
