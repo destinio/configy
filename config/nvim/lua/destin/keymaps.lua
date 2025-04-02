f501d33ebee41f35a4ec04a19c5733d9baeb913d
Destin Lee <dleeinc@gmail.com>
2025-04-02 14:28:17 -0500

    updates

--- config/nvim/lua/destin/keymaps.lua
+++ config/nvim/lua/destin/keymaps.lua
@@ -13,6 +13,8 @@ set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
 set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
+set('v', '<leader>fw', ":'<,'>!fmt -w 80<CR>", { desc = '[W]rap text 80' })
+
 -- Terminal mode
 set('t', 'jk', [[<C-\><C-n>]], opts)
 set('t', 'jj', [[<C-\><C-n>]], opts)
